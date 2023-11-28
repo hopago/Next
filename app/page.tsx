a911f17c6d6c3dbc2fff42fa2c50ec63c87c672f
hopago <132269386+hopago@users.noreply.github.com>
2023-11-28 17:12:37 +0900

    next blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-
+import Posts from "./components/Posts";
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Welcome,&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Hopago</span>
+        </span>
+      </p>
+      <Posts />
     </main>
   )
 }
---
+++ blogposts/ssg-ssr.md
@@ -0,0 +1,4 @@
+---
+title: 'When to Use Static Generation vs. Server-side Rendering'
+date: '2023-11-28'
+---
